750f7b5bc1d0b6115b1413421a1ea63833546106
J0J0C0DING <102717784+J0J0C0DING@users.noreply.github.com>
2023-01-23 19:38:07 -0600

    Initialized models User, Post, Comment, Vote

---
+++ tech-news-java-api/src/main/java/com/technews/VariableTypes.java
@@ -0,0 +1,11 @@
+package com.technews;
+
+public class VariableTypes {
+    public static void main(String[] args){
+        String name = "Joey";
+        int age = 23;
+        double price = 1999.99;
+        boolean truth = true;
+        boolean notTrue = false;
+    }
+}
---
+++ tech-news-java-api/src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,12 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args){
+        int num1 = 20;
+        double num2 = 19.99;
+        double sum = num1 + num2;
+        
+        String name = "Joey";
+        String sum2 = name + num1;
+    }
+}
